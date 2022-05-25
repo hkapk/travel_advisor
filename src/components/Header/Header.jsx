72dfe134f2343ee3e20218b193c7659562598836
Hank Kapka <hankkapka@gmail.com>
2022-05-25 11:11:28 -0500

    added google map API to display map

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,34 @@
 import React from 'react';
+import { Autocomplete } from '@react-google-maps/api';
+import { AppBar, Toolbar, Typography, InputBase, Box } from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
+
+import useStyles from './styles';
 
 const Header = () => {
+    const classes = useStyles();
     return (
-        <h1> Header </h1>
+        <AppBar position="static">
+            <Toolbar className={classes.toolbar}>
+                <Typography varient="h5" className={classes.title}>
+                    Trip Advisor
+                </Typography>
+                <Box display="flex">
+                <Typography varient="h5" className={classes.title}>
+                    Time to explore
+                </Typography>
+                {/* <Autocomplete> */}
+                    <div className={classes.search}>
+                        <div className={classes.searchIcon}>
+                            <SearchIcon />
+                        </div>
+                        <InputBase placeholder="Search..." classes={{root: classes.inputRoot, input: classes.inputInput}} />
+                    </div>
+                {/* </Autocomplete> */}
+                </Box>
+            </Toolbar>
+
+        </AppBar>
     );
 }
 
